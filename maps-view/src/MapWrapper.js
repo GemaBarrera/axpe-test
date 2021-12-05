a66d0cc115bf292e8e18e5633730502bb02a46ff
Gema <gbarrerafront@gmail.com>
2021-12-05 22:23:32 +0100

    create a wrapper component to render maps

---
+++ maps-view/src/MapWrapper.js
@@ -0,0 +1,18 @@
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+
+
+const render = (status) => {
+  if (status === Status.LOADING) return 'Loading...';
+  if (status === Status.FAILURE) return 'Error';
+  return null;
+};
+
+const MapWrapper = ({ mapComponent }) => {
+  return (
+    <Wrapper apiKey={"AIzaSyCrOHknAX3QjWVGDN2HNBBfsfBL8DXoJB4"} render={render}>
+      {mapComponent}
+    </Wrapper>
+  );
+};
+
+export default MapWrapper;
